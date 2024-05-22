16d462744ea03177e8e7cd2323e267f8a4b32f6d
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-05-22 12:47:10 +0200

    def create widgets to window frame(entry), added buttons to the frame with using lambda. used .grid to space buttons onn frame

--- gui.py
+++ gui.py
@@ -14,3 +14,52 @@ class CalculatorGUI:
         self.input_var.set("")
 
         self.create_widgets()
+
+    def create_widgets(self):
+        input_frame = tk.Frame(self.window)
+        input_frame.pack(padx=10, pady=10)
+
+        self.input_entry = tk.Entry(
+            input_frame,
+            textvariable=self.input_var,
+            font=("", 14),
+            bd=5,
+            justify="right",
+        )
+        self.input_entry.pack(fill="x")
+
+        button_frame = tk.Frame(self.window)
+        button_frame.pack(padx=10, pady=10)
+
+        buttons = [
+            ("7", lambda: self.append_to_input("7")),
+            ("8", lambda: self.append_to_input("8")),
+            ("9", lambda: self.append_to_input("9")),
+            ("+", lambda: self.append_to_input("+")),
+            ("4", lambda: self.append_to_input("4")),
+            ("5", lambda: self.append_to_input("5")),
+            ("6", lambda: self.append_to_input("6")),
+            ("-", lambda: self.append_to_input("-")),
+            ("1", lambda: self.append_to_input("1")),
+            ("2", lambda: self.append_to_input("2")),
+            ("3", lambda: self.append_to_input("3")),
+            ("*", lambda: self.append_to_input("*")),
+            ("C", self.clear_input),
+            ("0", lambda: self.append_to_input("0")),
+            ("/", lambda: self.append_to_input("/")),
+            ("=", self.calculate),
+        ]
+
+        for text, command in buttons:
+            button = tk.Button(
+                button_frame,
+                text=text,
+                font=("", 14),
+                padx=10,
+                pady=10,
+                command=command,
+            )
+            button.grid(
+                row=(buttons.index((text, command)) // 4),
+                column=(buttons.index((text, command)) % 4),
+            )
