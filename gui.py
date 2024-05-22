babe8aefd9d83718ce5e415af5a0e20ea7f68ebf
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-05-22 12:19:25 +0200

    created class, with window to display, and to refer to class calculator. added self.create_widgets

--- gui.py
+++ gui.py
@@ -1,3 +1,16 @@
 import tkinter as tk
 from main import *
 from main import Calculator
+
+
+class CalculatorGUI:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Calculator")
+
+        self.calc = Calculator()
+
+        self.input_var = tk.StringVar()
+        self.input_var.set("")
+
+        self.create_widgets()
