eab8bb6ad3c69889c0b629b5332bf8843c6dd988
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-05-23 15:14:07 +0200

    pull request (#2)
    * wip
    
    * fixed order of buttons
    
    * added spacing between the buttons and that the buttons match up perfectly
    
    * fixing adjustment to screen when expanding, buttons and screen input
    
    * pressing enter will calculate your calculation

--- gui.py
+++ gui.py
@@ -15,21 +15,26 @@ class CalculatorGUI:
 
         self.create_widgets()
 
+        self.window.grid_rowconfigure(0, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
+
     def create_widgets(self):
         input_frame = tk.Frame(self.window)
-        input_frame.pack(padx=10, pady=10)
+        input_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
         self.input_entry = tk.Entry(
             input_frame,
             textvariable=self.input_var,
             font=("", 14),
-            bd=5,
-            justify="right",
+            bd=10,
+            justify="center",
         )
-        self.input_entry.pack(fill="x")
+        self.input_entry.pack(fill="x", expand=True)
+
+        self.window.bind("<Return>", lambda event: self.calculate())
 
         button_frame = tk.Frame(self.window)
-        button_frame.pack(padx=10, pady=10)
+        button_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
         buttons = [
             ("7", lambda: self.append_to_input("7")),
@@ -39,30 +44,39 @@ class CalculatorGUI:
             ("4", lambda: self.append_to_input("4")),
             ("5", lambda: self.append_to_input("5")),
             ("6", lambda: self.append_to_input("6")),
-            ("-", lambda: self.append_to_input("-")),
+            ("- ", lambda: self.append_to_input("-")),
             ("1", lambda: self.append_to_input("1")),
             ("2", lambda: self.append_to_input("2")),
             ("3", lambda: self.append_to_input("3")),
-            ("*", lambda: self.append_to_input("*")),
-            ("C", self.clear_input),
+            ("x", lambda: self.append_to_input("*")),
             ("0", lambda: self.append_to_input("0")),
-            ("/", lambda: self.append_to_input("/")),
+            ("/ ", lambda: self.append_to_input("/")),
             ("=", self.calculate),
+            ("C", self.clear_input),
         ]
 
         for text, command in buttons:
             button = tk.Button(
                 button_frame,
                 text=text,
-                font=("", 14),
-                padx=10,
-                pady=10,
+                font=("", 10),
+                padx=5,
+                pady=5,
                 command=command,
             )
             button.grid(
                 row=(buttons.index((text, command)) // 4),
                 column=(buttons.index((text, command)) % 4),
+                padx=5,
+                pady=5,
+                sticky="nsew",
             )
+
+        # resize buttons to the size of user expand
+        for i in range(4):
+            button_frame.grid_rowconfigure(i, weight=1)
+        for i in range(4):
+            button_frame.grid_columnconfigure(i, weight=1)
 
     def append_to_input(self, value):
         current_input = self.input_var.get()
